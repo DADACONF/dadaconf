610b3e8ae5fc6cafee0e717760cf49816eb6e30d
Joey Yang <joey@Joeys-MacBook-Air-2.local>
2014-12-20 15:45:01 -0800

    install Firebase

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -2,16 +2,30 @@
   var NUM_FACES = 51;
   var FACE_PREFIX = "images/faces/ui_face"
   var FACE_EXTENSION = ".jpg"; 
+
+  var ref = new Firebase("https://dadaconf.firebaseio.com/");
+
   function randomUIFace() {
   	var faceIndex = Math.floor((Math.random() * NUM_FACES));
   	return FACE_PREFIX + faceIndex + FACE_EXTENSION;
   }
+
+  var setFaces = function(){
+    console.log("hello");
+    var face1 = $("#about-0");
+    var face2 = $("#about-1");
+    face1.attr("src", randomUIFace());
+    face2.attr("src", randomUIFace());
+  };
+
+  var initForm = function(){
+    
+  };
+
   $(document).ready(function() {
-  	console.log("hello");
-  	var face1 = $("#about-0");
-  	var face2 = $("#about-1");
-  	face1.attr("src", randomUIFace());
-  	face2.attr("src", randomUIFace());
+    setFaces();
+    initForm();
+
   });
   // on document ready insert two random photos as source for about us images. 
   // on load get 2 random photos and go for it
